19dc4d1c17206821ee2569147d0f41dff45d1d96
David Mudrak <david.mudrak@gmail.com>
2009-03-06 15:47:23 +0000

    Add missing strings

--- lang/en_utf8/groupselect.php
+++ lang/en_utf8/groupselect.php
@@ -1,25 +1,27 @@
 <?PHP // $Id$ 
 
 $string['action'] = 'Action';
+$string['fromallgroups'] = 'all groups';
 $string['groupselectname'] = 'Name';
 $string['headinggrouping'] = 'Group selection ($a)';
 $string['headingsimple'] = 'Group selection';
 $string['incorrectpassword'] = 'Incorrect password';
 $string['intro'] = 'Introduction';
-$string['modulename'] = 'Group selection';
-$string['modulenameplural'] = 'Group selections';
-$string['maxmembers'] = 'Max members per group';
 $string['managegroups'] = 'Manage groups';
+$string['maxmembers'] = 'Max members per group';
 $string['membercount'] = 'Count';
 $string['membershidden'] = 'Member list not available';
 $string['memberslist'] = 'Members';
+$string['modulename'] = 'Group selection';
+$string['modulenameplural'] = 'Group selections';
 $string['nogroups'] = 'No groups available to select from, sorry.';
+$string['noguestselect'] = 'Sorry, guests are not allowed to select groups.';
 $string['notavailableanymore'] = 'Group selection is not available anymore, sorry (since $a).';
 $string['notavailableyet'] = 'Group selection will be available on $a.';
-$string['noguestselect'] = 'Sorry, guests are not allowed to select groups.';
 $string['password'] = 'Signup password';
-$string['signup'] = 'Signup';
 $string['signupconfirm'] = 'Do you really want to signup as member of group <em>$a</em>?';
+$string['signup'] = 'Signup';
+$string['targetgrouping'] = 'Allow selection from';
 $string['timeavailable'] = 'Open from';
 $string['timedue'] = 'Open until';
 
