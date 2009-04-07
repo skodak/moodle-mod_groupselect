44b618363b6c9baf36ef7fedac4d7ff24c8e941d
Anna Vanova <anna.vanova@gmail.com>
2009-04-07 11:10:13 +0000

    group selection: CONTRIB-1148 bug fixed, student does not have to enter password when unenroling from a group

---
+++ signout_form.php
@@ -0,0 +1,25 @@
+<?php //$Id$
+
+require_once($CFG->dirroot.'/lib/formslib.php');
+
+class signout_form extends moodleform {
+
+    // Define the form
+    function definition () {
+        global $USER, $CFG, $COURSE;
+
+        $mform  =& $this->_form;
+        $groupselect = $this->_customdata;
+
+        $mform->addElement('hidden','id');
+        $mform->setType('id', PARAM_INT);
+
+        $mform->addElement('hidden','signout');
+        $mform->setType('signout', PARAM_INT);
+
+        $this->add_action_buttons(true, get_string('signout', 'groupselect'));
+    }
+
+}
+
+?>
