af7d1928a4e06dc190b9133b12db9b55dba0c316
rwijaya <rwijaya@moodle.com>
2010-11-23 12:04:43 +0800

    update input string

--- lang/en_utf8/groupselect.php
+++ lang/en_utf8/groupselect.php
@@ -1,9 +1,9 @@
-<?PHP // $Id$ 
+<?php
 
 $string['action'] = 'Action';
 $string['fromallgroups'] = 'all groups';
 $string['groupselectname'] = 'Name';
-$string['headinggrouping'] = 'Group selection ($a)';
+$string['headinggrouping'] = 'Group selection ({$a})';
 $string['headingsimple'] = 'Group selection';
 $string['incorrectpassword'] = 'Incorrect password';
 $string['intro'] = 'Introduction';
@@ -17,11 +17,11 @@ $string['modulenameplural'] = 'Group selections';
 $string['nogroups'] = 'No groups available to select from, sorry.';
 $string['noguestselect'] = 'Sorry, guests are not allowed to select groups.';
 $string['notavailable'] = 'Not available';
-$string['notavailableanymore'] = 'Group selection is not available anymore, sorry (since $a).';
-$string['notavailableyet'] = 'Group selection will be available on $a.';
+$string['notavailableanymore'] = 'Group selection is not available anymore, sorry (since {$a}).';
+$string['notavailableyet'] = 'Group selection will be available on {$a}.';
 $string['password'] = 'Enrol password';
-$string['signupconfirm'] = 'Do you really want to enrol as member of the group <em>$a</em>?';
-$string['signoutconfirm'] = 'Do you really want to unenrol from the group <em>$a</em>?';
+$string['signupconfirm'] = 'Do you really want to enrol as member of the group <em>{$a}</em>?';
+$string['signoutconfirm'] = 'Do you really want to unenrol from the group <em>{$a}</em>?';
 $string['signout'] = 'Unenrol';
 $string['signup'] = 'Enrol';
 $string['targetgrouping'] = 'Allow selection from';
