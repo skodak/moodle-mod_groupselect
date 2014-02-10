c0db692799608e5d1d0b7729c017d26dafaa638b
Alexander Kiy <alekiy@uni-potsdam.de>
2014-02-10 15:37:33 +0100

    added german translation

---
+++ lang/de/groupselect.php
@@ -0,0 +1,34 @@
+<?php
+
+$string['action'] = 'Aktion';
+$string['cannotselectclosed'] = 'Der Einschreibezeitraum ist abgelaufen.';
+$string['cannotselectmaxed'] = 'Einschreibung nicht möglich {$a} - das Maximum wurde erreicht.';
+$string['cannotselectnocap'] = 'Sie haben nicht die Berechigung, eine Gruppe zu wählen.';
+$string['cannotselectnoenrol'] = 'Sie müssen in den Kurs eingeschrieben sein, um Gruppenmitglied zu werden.';
+$string['cannotunselectclosed'] = 'Sie können die Gruppe nicht mehr verlassen.';
+$string['fromallgroups'] = 'Alle Gruppen';
+$string['groupselect:addinstance'] = 'Eine freie Gruppeneinteilung hinzufügen';
+$string['groupselect:select'] = 'Das Einschreiben in die Gruppe zulassen';
+$string['groupselect:unselect'] = 'Das Verlassen der Gruppe zulassen';
+$string['incorrectpassword'] = 'Falsches Passwort';
+$string['managegroups'] = 'Gruppeneinteilung bearbeiten';
+$string['maxlimitreached'] = 'Das Maximum der Gruppenmitglieder wurde erreicht.';
+$string['maxmembers'] = 'Maximale Mitglieder pro Gruppe';
+$string['membercount'] = 'Anzahl';
+$string['membershidden'] = 'Liste der Gruppenmitgleider nicht abrufbar';
+$string['memberslist'] = 'Gruppenmitglieder';
+$string['modulename'] = 'Freie Gruppeneinteilung';
+$string['modulenameplural'] = 'Freie Gruppeneinteilungen';
+$string['nogroups'] = 'Keine Gruppen vorhanden, aus den Sie wählen können.';
+$string['notavailableanymore'] = 'Die freie Gruppeneinteilung ist nicht mehr verfügbar (seit {$a}).';
+$string['notavailableyet'] = 'Die freie Gruppeneinteilung wird nutzbar sein ab {$a}.';
+$string['password'] = 'Passwort benötigt';
+$string['pluginadministration'] = 'Modul Administration';
+$string['pluginname'] = 'Freie Gruppeneinteilung';
+$string['selectconfirm'] = 'Wollen Sie sich wirklich in die Gruppe einschreiben <em>{$a}</em>?';
+$string['unselectconfirm'] = 'Wollen Sie sich wirklich aus der Gruppe austragen <em>{$a}</em>?';
+$string['unselect'] = 'Gruppe verlassen {$a}';
+$string['select'] = 'Gruppe beitreten {$a}';
+$string['targetgrouping'] = 'Aus Gruppierungen wählen';
+$string['timeavailable'] = 'Einschreibebeginn';
+$string['timedue'] = 'Einschreibeende';
